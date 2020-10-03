9ea076e32987a3799830838dc4c7b1d75c38b45c
Yu Wan <wanyuac@126.com>
2020-10-03 12:29:35 +0100

    Improve rmSeqAnnot.py

--- Script/rmSeqAnnot.py
+++ Script/rmSeqAnnot.py
@@ -11,11 +11,11 @@ becomes
 
 after using this script.
 
-Example command: cat seq.fna | python rmSeqDescr.py > seq_out.fna
+Example command: cat seq.fna | python rmSeqDescr.py 1> seq_out.fna
 
 Copyright (C) 2020 Yu Wan <wanyuac@126.com>
 Licensed under the GNU General Public Licence version 3 (GPLv3) <https://www.gnu.org/licenses/>.
-Publication: 15 June 2020; the latest modification: 5 September 2020
+Publication: 15 June 2020; the latest modification: 3 October 2020
 """
 
 import sys
@@ -24,8 +24,11 @@ def main():
     for line in sys.stdin:
         line = line.rstrip()
         if line.startswith(">"):
-            line, _ = line.split(" ", 1)  # Split the header by the first space character.
-        print(line)
+            try:
+                line, _ = line.split(" ", 1)  # Split the header by the first space character.
+            except ValueError:  # not enough values to unpack (expected 2, got 1)
+                print("Warning: Line %s is directly copied to the output as it does not have the annotation field." % line, file = sys.stderr)
+        print(line, file = sys.stdout)
 
     return
 
