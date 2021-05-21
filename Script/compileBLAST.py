423a612027a357d280995aba15c9751a3eb1170b
Yu Wan <wanyuac@126.com>
2021-05-22 00:48:59 +0100

    Major improvement

--- Script/compileBLAST.py
+++ Script/compileBLAST.py
@@ -1,28 +1,43 @@
 #!/usr/bin/env python
 
 """
-Compile and parse nucleotide BLAST+ outputs (outfmt 6) for a number of genomes. The output must be
-generated using command (which has been implemented in script screenGenes.pbs):
-blastn -query [FASTA file] -db [Database name] -out [sample name][delimiter][suffix].tsv -task megablast \
-    -evalue [1e-5] -perc_identity [70] -max_target_seqs [10] \
-    -outfmt '6 qseqid sseqid slen pident qcovhsp length mismatch gapopen qstart qend sstart send sstrand evalue bitscore sseq'
+Compile and parse nucleotide BLAST+ outputs (outfmt 6) for a number of genomes.
 
-Input: Files of BLAST+ outputs in output format 6.
-Outputs:
-    A tab-delimited table compiling hits in all sample genomes
-    FASTA files of subject DNA sequences, one file per query sequence
-    FASTA files of translated subject DNA sequences based on a given codon table (for bacterial genes: 11), one file per query sequence
+Input:
+    Files of BLAST+ outputs in output format 6 (TSV). The input TSV files must be generated using the following command,
+    which has been implemented in script screenGenes.pbs:
+
+    blastn -query [FASTA file] -db [Database name] -out [sample name][delimiter][suffix].tsv -task [megablast/blastn] \
+        -evalue [1e-5] -perc_identity [70] -max_target_seqs [10] \
+        -outfmt '6 qseqid sseqid slen pident qcovhsp length mismatch gapopen qstart qend sstart send sstrand evalue bitscore sseq'
+    
+    Note that the suffix in the output sample names is ignored by compileBLAST.py. However, the delimiter is used for extracting
+    sample names from input filenames.
+
+Outputs: (created under the current working directory)
+    (1) *__aln.tsv: A tab-delimited table compiling hits in all sample genomes
+    (2) *.fna: FASTA files of subject DNA sequences, one file per query sequence
+    (3) *.faa (optional): FASTA files of translated subject DNA sequences based on a given codon table (for bacterial genes: 11),
+        one file per query sequence.
+
+Note:
+    Path to a text file can be given to the argument '--genes' using the syntax: '--genes File:$file_path'. The text file ($file_path)
+    lists a gene name per line. The list can be easily generated using the command:
+    cat *.fna | grep '>' | cut -d ' ' -f 1 | sed 's/>//g' > genes.txt  # The 'cut' command removes sequence annotations.
+
 Example command:
-    python compileBLAST.py --input *__megaBLAST.tsv --delimiter '__' --genes 'gene1,gene2,gene3' --output study1 --translate --codon_table 11 --add_sample_name > compile_blast.log
+    python compileBLAST.py --input *__megaBLAST.tsv --delimiter '__' --genes 'gene1,gene2,gene3' --output study1 --translate \
+        --codon_table 11 --add_sample_name > compile_blast.log
 Dependencies: Python 3, BioPython
 
 Copyright (C) 2020 Yu Wan <wanyuac@126.com>
 Licensed under the GNU General Public Licence version 3 (GPLv3) <https://www.gnu.org/licenses/>.
-Publication: 11 June 2020; the latest modification: 22 July 2020
+Publication: 11 June 2020; the latest modification: 22 May 2021
 """
 
 import os
 import re
+import sys
 from collections import namedtuple
 from argparse import ArgumentParser
 from Bio.Seq import Seq
@@ -36,28 +51,28 @@ def parse_arguments():
     parser.add_argument('--delimiter', '-d', dest = 'delimiter', type = str, required = True, \
         help = 'Delimiter character(s) separating the sample name and the suffix in every input filename')
     parser.add_argument('--genes', '-g', dest = 'genes', type = str, required = True, \
-        help = 'Comma delimited vector of gene names (BLAST queries) in BLAST output files')
+        help = 'A comma-delimited vector or a one-column text file for gene names (BLAST queries) in BLAST output files')
     parser.add_argument('--output', '-o', dest = 'output', type = str, required = False, default = 'blast', \
         help = 'Common prefix of output files. Default: blast')
     parser.add_argument('--translate', '-t', dest = 'translate', action = "store_true", required = False,\
-        help = 'Translate DNA sequences into protein sequences')
+        help = 'Translate DNA sequences into protein sequences using a specific codon table')
     parser.add_argument('--codon_table', '-c', dest = 'codon_table', required = False, type = int, default = 11, \
         help = 'Index of codon tables for translating coding sequences into protein sequences')
-    parser.add_argument('--add_sample_name', '-a', action = "store_true", required = False,\
-        help = 'Append a sample name to each sequence ID')
+    parser.add_argument('--add_sample_name', '-a', action = 'store_true', required = False,\
+        help = 'Append a sample name to each sequence ID (useful for sequence clustering)')
 
     return parser.parse_args()
 
 
 HIT_ATTRS = ['qseqid', 'sseqid', 'slen', 'pident', 'qcovhsp', 'length', 'mismatch', 'gapopen', 'qstart', 'qend',\
-             'sstart', 'send', 'sstrand', 'evalue', 'bitscore']  # 15 attribute fields
+             'sstart', 'send', 'sstrand', 'evalue', 'bitscore']  # 15 attribute fields of interest
 
 
 def main():
     args = parse_arguments()
 
     # Create output files, keep them open, and save their handles
-    genes = args.genes.split(',')
+    genes = make_gene_list(args.genes)
     out_tsv = open(args.output + '__aln.tsv', 'w')  # A summary of alignments
     out_tsv.write('\t'.join(['sample'] + HIT_ATTRS) + '\n')  # The write method does not automatically append a newline character to the end of the output line.
     out_fna_handles = {}
@@ -124,14 +139,11 @@ def main():
         out_fna_handles[g].close()
         if args.translate:
             out_faa_handles[genes_prot[g]].close()
-
     return
 
 
 def parse_blast_line(line):
-    """
-    Parse each line of BLAST outputs into a Hit object (namedtuple)
-    """
+    """ Parse each line of BLAST outputs into a Hit object (namedtuple) """
     Hit = namedtuple("Hit", HIT_ATTRS + ['sseq'])
     fields = line.split('\t')
 
@@ -141,18 +153,29 @@ def parse_blast_line(line):
               length = fields[5], mismatch = fields[6], gapopen = fields[7], qstart = fields[8], qend = fields[9],\
               sstart = fields[10], send = fields[11], sstrand = '+' if fields[12] == 'plus' else '-', evalue = fields[13],\
               bitscore = fields[14], sseq = re.sub('-', '', fields[15]))
-
     return hit
 
 
 def extract_sample_name(filename, delim):
-    """
-    Extract sample name from a file name.
-    """
+    """ Extract sample name from a file name. """
     b = os.path.basename(filename)
     sample_name = b.split(delim)[0]
-
     return sample_name
+
+
+def make_gene_list(s):
+    """ Parse the argument --genes """
+    if s.startswith("File:"):
+        s = s[5 : ]  # Drop 'File:'
+        if os.path.exists(s):
+            with open(s, 'r') as f:
+                gs = f.read().splitlines()
+        else:
+            print('Error: gene-list file ' + s + " is not accessible.")
+            sys.exit(1)
+    else:
+        gs = s.split(',')
+    return gs
 
 
 def write_fasta(file_handle, seq_rec):
@@ -162,7 +185,6 @@ def write_fasta(file_handle, seq_rec):
     """
     print('>%s %s' % (seq_rec.id, seq_rec.description), file = file_handle)
     print(seq_rec.seq, file = file_handle)
-
     return
 
 
